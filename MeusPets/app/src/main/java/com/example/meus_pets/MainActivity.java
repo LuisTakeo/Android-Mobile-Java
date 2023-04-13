3faac62d93585383c21c52ee4cc034d5d2b640dd
LuisTakeo <luistakeo@hotmail.com>
2023-04-12 22:23:34 -0300

    Projeto de cadastro de pets

---
+++ MeusPets/app/src/main/java/com/example/meus_pets/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.meus_pets;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnGoToSecondActivity;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnGoToSecondActivity = findViewById(R.id.btn_go_to_second_activity);
+        btnGoToSecondActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
